1a1c1d76cf5ee292d5618c89aca61bce865f52e9
Danika Peet <DPeet@lcs.on.ca>
2024-06-11 16:18:01 -0400

    Initial Commit

---
+++ Pro-Planner/Pro_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_PlannerApp.swift
+//  Pro-Planner
+//
+//  Created by Danika Peet on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Pro_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
